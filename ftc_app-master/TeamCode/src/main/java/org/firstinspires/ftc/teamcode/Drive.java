4b09d65ededa72ff0c0e844161ebc6ada515d70a
Angie Cortez <33558160+Error856@users.noreply.github.com>
2018-02-17 09:28:27 -0500

    Update Drive.java

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -18,79 +19,55 @@ public class Drive extends OpMode{
     DcMotor frontLeft;
     DcMotor backLeft;
     DcMotor backRight;
-    DcMotor glyphMotor;
-    DcMotor treadR;
     DcMotor treadL;
-    
-    Servo servoJ;
-    
-   
-    public void init(){
+    DcMotor treadR;
+    DcMotor glyphMotor;
 
-        frontRight = hardwareMap.dcMotor.get("frontRightMotor");
-        frontLeft = hardwareMap.dcMotor.get("frontLeftMotor");
-        backRight = hardwareMap.dcMotor.get("backRightMotor");
-        backLeft = hardwareMap.dcMotor.get("backLeftMotor");
-        glyphMotor = hardwareMap.dcMotor.get("glyphMotor");
+    Servo servoJ;
+
+    public void init(){
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
         treadL = hardwareMap.dcMotor.get("treadL");
         treadR = hardwareMap.dcMotor.get("treadR");
-        
-        servoJ = hardwareMap.servo.get("sevoJ");
+        glyphMotor = hardwareMap.dcMotor.get("glyphMotor");
+        servoJ = hardwareMap.servo.get("servoJ");
 
     }
 
-    public void loop() {
+        public void loop() {
 
-        //In place of motor power, gamestick position is used
-        float move = -gamepad1.left_stick_y;
-        float rotation = gamepad1.right_stick_x;
-        float crabWalk = -gamepad1.left_stick_x;
+            //In place of motor power, gamestick position is used
+            float move = -gamepad1.left_stick_y;
+            float rotation = gamepad1.right_stick_x;
+            float crabWalk = -gamepad1.left_stick_x;
 
-        //Wheels: Holonomic drive formula uses values of gamestick position to move
-        double fLeftPower = Range.clip(move + rotation - crabWalk, -1.0, 1.0);
-        double bLeftPower = Range.clip(move + rotation + crabWalk, -1.0, 1.0);
-        double fRightPower = Range.clip(move - rotation + crabWalk, -1.0, 1.0);
-        double bRightPower = Range.clip(move - rotation - crabWalk, -1.0, 1.0);
+            //Wheels: Holonomic drive formula uses values of gamestick position to move
+            double fLeftPower = Range.clip(move + rotation - crabWalk, -1.0, 1.0);
+            double bLeftPower = Range.clip(move + rotation + crabWalk, -1.0, 1.0);
+            double fRightPower = Range.clip(move - rotation + crabWalk, -1.0, 1.0);
+            double bRightPower = Range.clip(move - rotation - crabWalk, -1.0, 1.0);
 
-        //Assignment of motor power in relation to wheels
-        frontRight.setPower(fLeftPower/2);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setPower(bLeftPower/2);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setPower(fRightPower/2);
-        backRight.setPower(bRightPower/2);
-        
-        //Assignment of motor power in relation to glyph intake mechanism
-        glyphMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
-        
+            //Assignment of motor power in relation to wheels
+            frontLeft.setPower(fLeftPower/.5);
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backLeft.setPower(bLeftPower/.5);
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontRight.setPower(fRightPower/.5);
+            backRight.setPower(bRightPower/.5);
 
-        if (gamepad1.left_bumper){
-            glyphMotorL.setPower(1);
-            glyphMotorR.setPower(1);
+
+
+            treadR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            treadL.setPower(gamepad2.left_trigger);
+            treadR.setPower(gamepad2.left_trigger);
+            treadL.setPower(-gamepad2.right_trigger);
+            treadR.setPower(-gamepad2.right_trigger);
+
+            glyphMotor.setPower(-gamepad2.right_stick_y/2);
+
+           }
         }
-
-            else {
-                glyphMotorL.setPower(0);
-                glyphMotorR.setPower(0);
-        }
-        
-        //Assignment of motor power in relation to glyph
-        if (gamepad1.right_trigger){
-            treadL.setPower(1);
-            treadR.setPower(1);
-        }
-        
-        else if (gamepad1.left_trigger){
-            treadL.setPower(-1);
-            treadR.setPower(-1);
-        }
-        
-        else {
-            treadMotorL.setPower(0);
-            treadMotorR.setPower(0);
-        
-
-    }
-
-
-}
